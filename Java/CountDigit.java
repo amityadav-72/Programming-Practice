410f5bd3d70b8b835ecdad18db22267a4b3c6050
Amit Kumar Yadav <ay6732716@gmail.com>
2025-10-01 23:01:48 +0530

    Update CountDigit.java

--- Java/CountDigit.java
+++ Java/CountDigit.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class CountDigit {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int count=0;
+
+        for(int i=0; i<number; i++){
+
+            count = 0;
+
+            int temp = number;
+
+            while(temp!=0){
+
+                temp /= 10;
+
+                count++;
+            }
+
+
+        }
+        System.out.println("The number of Digit in "+ number + " is " +count);
+    }
 }
