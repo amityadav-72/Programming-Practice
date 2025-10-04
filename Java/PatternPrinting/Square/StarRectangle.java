bb30af64c206632739ce487316d6f9f4ddab0ff1
Amit Kumar Yadav <ay6732716@gmail.com>
2025-10-04 21:09:56 +0530

    Create StarRectangle.java

---
+++ Java/PatternPrinting/Square/StarRectangle.java
@@ -0,0 +1,23 @@
+package PatternPrinting.Square;
+import java.util.*;
+
+public class StarRectangle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Number of Rows");
+        int rows = sc.nextInt();
+        System.out.println("Enter the Number of Columns");
+        int col = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= col; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); // move to next line after each row
+        }
+
+        sc.close();
+    }
+}
