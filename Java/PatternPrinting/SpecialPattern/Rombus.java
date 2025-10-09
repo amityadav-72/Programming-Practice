b7dc074943fc00d94ffd7e8d5329901f68a96705
Amit Kumar Yadav <ay6732716@gmail.com>
2025-10-09 22:53:59 +0530

    Create Rombus.java

---
+++ Java/PatternPrinting/SpecialPattern/Rombus.java
@@ -0,0 +1,32 @@
+package PatternPrinting.SpecialPattern;
+import java.util.Scanner;
+
+
+
+public class Rombus {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of line: ");
+        int num =sc.nextInt();
+
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=num-i; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=num; j++){
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+
+//Enter the number of line: 4
+//              * * * *
+//            * * * *
+//          * * * *
+//        * * * * 
