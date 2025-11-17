cd668122f402159b8fd0e179d0f38cbc69d9438a
Amit Kumar Yadav <ay6732716@gmail.com>
2025-11-17 22:09:08 +0530

    Update InputOfArray.java

--- Java/Arrays/InputOfArray.java
+++ Java/Arrays/InputOfArray.java
@@ -21,5 +21,15 @@ public class InputOfArray {
         for(int i =0; i<5; i++){
             System.out.print(arr[i]+ " ");
         }
+
+
+        System.out.println("Second Array direct with curly bracket: ");
+
+        int[] arr1 = {1,2,3,4,5};
+
+        for(int i=0; i<5; i++){
+            System.out.print(arr1[i]+ " ");
+        }
+
     }
 }
