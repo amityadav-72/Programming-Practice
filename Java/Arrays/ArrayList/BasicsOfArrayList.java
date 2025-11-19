155045ddeef79c82eec452757201d4919b893362
Amit Kumar Yadav <ay6732716@gmail.com>
2025-11-19 22:05:26 +0530

    Create BasicsOfArrayList.java

---
+++ Java/Arrays/ArrayList/BasicsOfArrayList.java
@@ -0,0 +1,17 @@
+package Arrays.ArrayList;
+import java.util.ArrayList;
+
+public class BasicsOfArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr= new ArrayList<>();
+
+        arr.add(0,10);
+        arr.add(1,20);
+        arr.add(2,30);
+        arr.add(3,40);
+        arr.add(4,50);
+        arr.add(5,60);
+
+        System.out.println(arr);
+    }
+}
